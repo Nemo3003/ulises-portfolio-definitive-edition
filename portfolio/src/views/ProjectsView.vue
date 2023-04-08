7e8381dc80e883ce018a50db322df255c9a2b001
Nemo3003 <ulimarian2000@gmail.com>
2023-04-08 20:04:09 -0300

    [UPD] Titles and descriptions added

--- portfolio/src/views/ProjectsView.vue
+++ portfolio/src/views/ProjectsView.vue
@@ -5,78 +5,84 @@
     <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>First</h2>
+      <h2>Quiz Chaco</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>Quiz Chaco</h6>
+      <p>Django + Bootstrap</p>
+      <p>Website in which it is possible to sign up and sign in. It includes a game with a few questions about the province of Chaco</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Second</h2>
+      <h2>Puerto Tirol: Chamber of commerce</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>Puerto Tirol: Chamber of commerce</h6>
+      <p>HTML + CSS + JS</p>
+      <p>DOM manipulation, Mobile First Design, API connection, Grid, Flex, SEO</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>Temple Inn & Suites</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>Temple Inn & Suites</h6>
+      <p>HTML + CSS + JS</p>
+      <p>The Temple Inn & Suites is a website for a fake hotel chain
+        - API usage
+        - SEO
+        - Lightweight 
+        - Dynamic Page
+        - JSON
+      </p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>Linkedin-Scrapper, notification-based</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>Linkedin-Scrapper, notification-based</h6>
+      <p>Google extension - JS</p>
+      <p>This is a light-weight scrapper that only takes the name, profession and job from a certain amount of people</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>Notes App</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>Notes App</h6>
+      <p>React + Bootstrap + Firebase</p>
+      <p>A notes app that allows you to write a note, delete it, edit it, or archive it.</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>GIF App</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>GIF App</h6>
+      <p>React + Bootstrap</p>
+      <p>This app will allow you to search for a GIF</p>
     </div>
   </div>
 </div>
@@ -84,13 +90,39 @@
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>Heroes APP</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>HEROES APP</h6>
+      <p>React + Typescript + firebase</p>
+      <p>Consumption of API using React and firebase. It allows you to search for heroes</p>
+    </div>
+  </div>
+</div>
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <h6>Gym APP</h6>
+      <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>GYM App</h1>
+      <p>.NET + Bootstrap + Angular</p>
+      <p>An app that allows a gym to manage their classes and allows the students to choose in which one to enroll</p>
+    </div>
+  </div>
+</div>
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <h2>Rock, Paper Scissors</h2>
+      <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h6>Rock, Paper, Scissors</h6>
+      <p>Golang + Bootstrap + JS</p>
+      <p>A game using Golang and bootstrap </p>
     </div>
   </div>
 </div>
@@ -101,7 +133,7 @@
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
+      <h6>John Doe</h6>
       <p>Architect & Engineer</p>
       <p>We love that guy</p>
     </div>
@@ -110,104 +142,91 @@
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>Fina App</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>Fina App</h6>
+      <p>React + Bootstrap + Firebase</p>
+      <p>An E-commerce with voice recognition!</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>PHP Motors</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>PHP Motors</h6>
+      <p>PHP + MySQL + CSS</p>
+      <p>This is a website in which you can navigate among the different vehicles this website offers</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>Videogames site</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>Videogames Site</h6>
+      <p>Handlebars + Node.Js + MongoDB</p>
+      <p>This is a page that allows you to look between different videogames</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>Sonografico</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>Sonografico</h6>
+      <p>Unity + C#</p>
+      <p>Here I worked in the sounds for this interactive videogame</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>Laravel Management App</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>Laravel Management App</h6>
+      <p>Laravel + MySQL + Bootstrap + CSS</p>
+      <p>This app allows you to create a company, then relate its branches to it and add employees to those branches</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h2>First Discord Bot</h2>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>First Discord Bot</h6>
+      <p>C#</p>
+      <p> I was tired of having to choose between different classes at university to complete homework, and also I had the issue that I have too many games to choose from when playing with friends, so I made a Discord Bot to help me out with those decisions</p>
     </div>
   </div>
 </div>
 <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <h2>Default</h2>
+      <h4>TO-DO App</h4>
       <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
-    </div>
-  </div>
-</div>
-<div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <h2>Default</h2>
-      <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+      <h6>TO-DO App</h6>
+      <p>Vue.Js + Bootstrap + Firebase</p>
+      <p>This to-do app allows you to save your tasks and to mark them as "to-do", "in-progress", and "finished" In order to get into this place you need to register with either an email and password or with your google account, which is managed by firebase</p>
     </div>
   </div>
 </div>
